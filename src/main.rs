9aeed99cfa9fe3fe79e139c9b7cc921ebf422064
michelevantaggi02 <michelevantaggi02@gmail.com>
2024-12-01 15:49:33 +0100

    Base

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+/**
+ * Advent of Code 2024
+ * https://adventofcode.com/2024
+ * 
+ * @Author: Michele
+ * 
+ * This is the main file that will run the exercises for each day
+ * Every day will have its own module with the exercises, called dayX
+ * Every module will have its own mod.rs file with the exercises
+ * Every exercise will have its own function and a run() function to call both
+ */
+
+
+fn main() {
+}
